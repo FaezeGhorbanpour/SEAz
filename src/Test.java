e3649a9107df1cd5fb2a50d49cd1469aa945ed88
FaezeGhorbanpour <f.gorbanpor@yahoo.com>
2018-03-07 23:56:59 +0330

    add multiply test

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,21 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+    private static void testMultiply()
+    {
+        int a = 5;
+        int b = 9;
+        int expected = 45;
+        BasicMath bm = new BasicMath();
+        int result = bm.sum(a, b);
+        if (result == expected)
+        {
+            System.out.println("Sum: OK");
+        }
+        else
+        {
+            System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
+    
 }
